a808b1866eb16691b6d6133c461f04e6ab7db5af
davindb <davindbhagaspati@gmail.com>
2021-01-14 13:29:10 +0700

    1. Constructor Function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+// 1. Constructor Function
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, my name is Davin. I want to learn React.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
